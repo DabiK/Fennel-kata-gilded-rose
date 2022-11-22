f7e3adde472556a63aa0623760cb8eecfebf8086
Dabi <keitadk9@gmail.com>
2022-11-22 11:27:46 +0100

    increaseQuality and decreaseQuality function

--- javascript/src/gilded_rose.js
+++ javascript/src/gilded_rose.js
@@ -1,10 +1,25 @@
 const { AGED_BRIE, BACKSTAGE_PASSES_TO_A_TAF,SULFURAS } = require("./product");
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
 
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
+  }
+  increaseQuality(){
+    if(this.quality < MAX_QUALITY)
+      this.quality++;
+  }
+
+  decreaseQuality(){
+    if(this.quality > MIN_QUALITY)
+      this.quality--;
+  }
+
+  setQualityToZero(){
+    this.quality = 0;
   }
 }
 
@@ -16,57 +31,46 @@ class Shop {
   /*
   /!\ Do not change code above this line /!\
   */
-
-  updateQuality() {
-    this.items.forEach((item) => {
-      if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES_TO_A_TAF) {
-        if (item.quality > 0) {
-          if (item.name != SULFURAS) {
-            item.quality = item.quality - 1;
+updateQuality() {
+  this.items.forEach((item) => {
+    if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES_TO_A_TAF) {
+      if (item.name != SULFURAS) {
+        item.decreaseQuality();
+      }
+    } else {
+      if (item.quality < MAX_QUALITY) {
+        item.increaseQuality();
+        if (item.name == BACKSTAGE_PASSES_TO_A_TAF) {
+          if (item.sellIn < 11) {
+            item.increaseQuality();
           }
+          if (item.sellIn < 6) {
+            item.increaseQuality();
+          }
+        }
+      }
+    }
+    if (item.name != SULFURAS) {
+      item.sellIn = item.sellIn - 1;
+    }
+    if (item.sellIn < 0) {
+      if (item.name != AGED_BRIE) {
+        if (item.name != BACKSTAGE_PASSES_TO_A_TAF) {
+          if (item.name != SULFURAS) {
+            item.decreaseQuality();
+          } 
+        } else {
+          item.setQualityToZero();
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == BACKSTAGE_PASSES_TO_A_TAF) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
+          item.increaseQuality();
       }
-      if (item.name != SULFURAS) {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != AGED_BRIE) {
-          if (item.name != BACKSTAGE_PASSES_TO_A_TAF) {
-            if (item.quality > 0) {
-              if (item.name != SULFURAS) {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-    })
-    return this.items;
-  }
+    }
+  })
+  return this.items;
+}
 }
 module.exports = {
-  Item,
-  Shop
+Item,
+Shop
 }
