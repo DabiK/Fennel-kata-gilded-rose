cac8d250dea0d56b4d2295a2acf694ad3a861ea7
Dabi <keitadk9@gmail.com>
2022-11-22 11:46:37 +0100

    handle special product quality

--- javascript/src/gilded_rose.js
+++ javascript/src/gilded_rose.js
@@ -1,82 +1,80 @@
-const { AGED_BRIE, BACKSTAGE_PASSES_TO_A_TAF,SULFURAS } = require("./product");
+const { AGED_BRIE, BACKSTAGE_PASSES_TO_A_TAF, SULFURAS } = require("./product");
 const MAX_QUALITY = 50;
 const MIN_QUALITY = 0;
-const LIMIT_SELLIN =0;
+const LIMIT_SELLIN = 0;
 
 class Item {
-  constructor(name, sellIn, quality,maxQuality = MAX_QUALITY){
+  constructor(name, sellIn, quality, maxQuality = MAX_QUALITY) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
-    this.maxQuality = maxQuality
+    this.maxQuality = maxQuality;
   }
-  increaseQuality(){
-    if(this.quality < this.maxQuality)
-      this.quality++;
-  }
-
-  decreaseQuality(){
-    if(this.quality > MIN_QUALITY)
-      this.quality--;
+  increaseQuality() {
+    if (this.quality < this.maxQuality) this.quality++;
   }
 
-  setQualityToZero(){
+  decreaseQuality() {
+    if (this.quality > MIN_QUALITY) this.quality--;
+  }
+
+  setQualityToZero() {
     this.quality = 0;
   }
 
-  decreaseSellIn(){
-    this.sellIn --;
+  decreaseSellIn() {
+    this.sellIn--;
+  }
+
+  isNameEqual(names) {
+    return names.include(this.name);
   }
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
 
   /*
   /!\ Do not change code above this line /!\
   */
-updateQuality() {
-  this.items.forEach((item) => {
-    if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES_TO_A_TAF) {
-      if (item.name != SULFURAS) {
-        item.decreaseQuality();
-      }
-    } else {
-      if (item.quality < MAX_QUALITY) {
-        item.increaseQuality();
-        if (item.name == BACKSTAGE_PASSES_TO_A_TAF) {
-          if (item.sellIn < 11) {
-            item.increaseQuality();
-          }
-          if (item.sellIn < 6) {
-            item.increaseQuality();
-          }
-        }
-      }
-    }
-    if (item.name != SULFURAS) {
-      item.decreaseSellIn();
-    }
-    if (item.sellIn < LIMIT_SELLIN) {
-      if (item.name != AGED_BRIE) {
-        if (item.name != BACKSTAGE_PASSES_TO_A_TAF) {
-          if (item.name != SULFURAS) {
-            item.decreaseQuality();
-          } 
-        } else {
-          item.setQualityToZero();
+  updateQuality() {
+    this.items.forEach((item) => {
+      if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES_TO_A_TAF) {
+        if (item.name != SULFURAS) {
+          item.decreaseQuality();
         }
       } else {
+        if (item.quality < MAX_QUALITY) {
           item.increaseQuality();
+          if (item.name == BACKSTAGE_PASSES_TO_A_TAF) {
+            if (item.sellIn < 11) {
+              item.increaseQuality();
+            }
+            if (item.sellIn < 6) {
+              item.increaseQuality();
+            }
+          }
+        }
       }
-    }
-  })
-  return this.items;
-}
+      if (item.name != SULFURAS) {
+        item.decreaseSellIn();
+      }
+      if (item.sellIn < LIMIT_SELLIN) {
+        if (item.name == AGED_BRIE) {
+          item.increaseQuality();
+        } else if (item.name == BACKSTAGE_PASSES_TO_A_TAF) {
+          item.setQualityToZero();
+        } else if (item.name != SULFURAS) {
+          item.decreaseQuality();
+        }
+      }
+    });
+    return this.items;
+  }
 }
 module.exports = {
-Item,
-Shop
-}
+  Item,
+  Shop,
+};
--- javascript/src/product.js
+++ javascript/src/product.js
@@ -1,9 +1,9 @@
-const AGED_BRIE = 'Aged Brie';
-const SULFURAS = 'Sulfuras, Hand of Ragnaros'
-const BACKSTAGE_PASSES_TO_A_TAF = 'Backstage passes to a TAFKAL80ETC concert'
+const AGED_BRIE = "Aged Brie";
+const SULFURAS = "Sulfuras, Hand of Ragnaros";
+const BACKSTAGE_PASSES_TO_A_TAF = "Backstage passes to a TAFKAL80ETC concert";
 
-module.exports =  {
-    AGED_BRIE, 
-    SULFURAS, 
-    BACKSTAGE_PASSES_TO_A_TAF
+module.exports = {
+  AGED_BRIE,
+  SULFURAS,
+  BACKSTAGE_PASSES_TO_A_TAF,
 };
